8e7df95fda3542d015c044724d3215e8cf53a2af
柳澤郁哉 <fumiya.shu@gmail.com>
2018-02-20 01:17:26 +0900

    commentモデル/テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219161122) do
+ActiveRecord::Schema.define(version: 20180219161641) do
+
+  create_table "comments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "contents", force: :cascade do |t|
     t.datetime "created_at", null: false
