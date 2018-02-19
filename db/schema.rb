46a2f65e511042ffb4dfb988543216fd17c9dc62
柳澤郁哉 <fumiya.shu@gmail.com>
2018-02-20 01:13:32 +0900

    Rake dbしたよ

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219154226) do
+ActiveRecord::Schema.define(version: 20180219161122) do
+
+  create_table "contents", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
