b5a715d4ca0679dcb5752cbf872c54d32ab66321
柳澤郁哉 <fumiya.shu@gmail.com>
2018-03-11 12:54:35 +0900

    no message

--- db/migrate/20180219181518_remove_course_name_from_contents.rb
+++
@@ -1,7 +0,0 @@
-class RemoveCourseNameFromContents < ActiveRecord::Migration
-  def change
-    remove_column :contents, :course_name, :string
-    remove_column :contents, :menu1, :string
-    remove_column :contents, :menu2, :string
-  end
-end
